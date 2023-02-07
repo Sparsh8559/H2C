b8dda480ceb05bd03c45e6ed9e68111b366fecb1
Sparsh <sparsh8559@gmail.com>
2023-02-07 22:09:46 +0530

    made for pre submission

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -21,7 +21,15 @@ class _homeScreenState extends State<homeScreen> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text('Hi,\nUser',style: TextStyle(color: Colors.deepPurple,fontSize: 26),),
-
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Select Date',style:TextStyle(fontSize: 20),)
+                        ElevatedButton(onPressed: ()async {
+                          DateTime? datePicked= await showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(2022), lastDate: DateTime(2024))
+                        }, child: Text('Date'))
+                      ],
+                    )
 
                   ],
 
@@ -32,6 +40,9 @@ class _homeScreenState extends State<homeScreen> {
             Expanded(
               flex: 4,
               child: Container(
+                child: Center(
+                  child:Image.asset('assets/message-130659740-14367398737171749148.jpg'),
+                ),
 
 
               ),
@@ -40,6 +51,10 @@ class _homeScreenState extends State<homeScreen> {
               flex: 2,
               child: Container(
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: (){}, child: Text('Download'))
+                  ],
 
                 ),
 

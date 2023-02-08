143c06957b43a71ddf24994abccb6929ce9e4521
Sparsh <sparsh8559@gmail.com>
2023-02-08 18:46:51 +0530

    new  commit 1

--- lib/map.dart
+++ lib/map.dart
@@ -16,11 +16,22 @@ class _mapRouteState extends State<mapRoute> {
   static final CameraPosition _kgoogleplex = const CameraPosition(target:
   LatLng(12.9716,  77.5946),
   zoom: 13,);
+
+
   List<Marker> _marker =[];
   List<Marker> _list =[
     Marker(markerId: MarkerId('1'),
     position: LatLng(12.9716,  77.5946),
-    infoWindow: InfoWindow(title: 'Current Location'))
+    infoWindow: InfoWindow(title: 'Current Location')),
+    Marker(markerId: MarkerId('2'),
+        position: LatLng(13.9716,  76.5946),
+        infoWindow: InfoWindow(title: 'My location')),
+    Marker(markerId: MarkerId('3'),
+        position: LatLng(11.9716,  75.5946),
+        infoWindow: InfoWindow(title: 'Location')),
+    Marker(markerId: MarkerId('4'),
+        position: LatLng(12.9716,  77.5946),
+        infoWindow: InfoWindow(title: 'Current'))
   ];
 
   @override
@@ -33,15 +44,30 @@ class _mapRouteState extends State<mapRoute> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: _kgoogleplex,
-        markers: Set<Marker>.of(_marker),
-        onMapCreated: (GoogleMapController Controller){
-          _controller.complete(Controller);
-        },
-        myLocationEnabled: true,
-        compassEnabled: true,
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: _kgoogleplex,
+          markers: Set<Marker>.of(_marker),
+          onMapCreated: (GoogleMapController Controller){
+            _controller.complete(Controller);
+          },
+          myLocationEnabled: true,
+          compassEnabled: true,
+        ),
       ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(Icons.location_disabled_outlined),
+      //   onPressed: ()async{
+      //     GoogleMapController Controller =await _controller.future;
+      //     Controller.animateCamera(CameraUpdate.newCameraPosition(
+      //       CameraPosition(target: LatLng(12.9716,  77.5946),
+      //       zoom: 13)
+      //     ));
+      //     setState(() {
+      //
+      //     });
+      //   },
+      // ),
     );
   }
 }
